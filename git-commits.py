aa1e295fa5cd56ca9e62402ce636e0b9da38e67c
Daniel Fleischer <danflscr@gmail.com>
2021-03-23 12:49:18 +0200

    Cleaning

--- git-commits.py
+++ git-commits.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-
-import git, sys, os
+import git
+import sys
+import os
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -9,16 +9,17 @@ project = sys.argv[1]
 repo = git.repo.Repo(project)
 
 data = []
-
 for i in reversed(list(repo.iter_commits())):
     diff = i.stats.total
-    data.append([i.committed_datetime.isoformat(), diff['insertions'], diff['deletions']])
+    data.append([i.committed_datetime.isoformat(),
+                 diff['insertions'],
+                 diff['deletions']])
 
-data = pd.DataFrame(data, columns=["date","add","remove"])
+data = pd.DataFrame(data, columns=["date", "add", "remove"])
 data['delta'] = data['add'] - data['remove']
 data['total'] = data.delta.cumsum()
-data.date = pd.to_datetime(data['date'])
-data.set_index(['date'],inplace=True)
+data.date = pd.to_datetime(data.date)
+data.set_index(['date'], inplace=True)
 
 plt.figure(f"Code Lines Progress in project {os.path.basename(project)}")
 plt.ylabel("# of lines")
