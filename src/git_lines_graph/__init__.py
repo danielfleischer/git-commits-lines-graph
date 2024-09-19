3fd1dc5dd9ef53a7ee70083884279bf9ad5ed17e
Daniel Fleischer <danflscr@gmail.com>
2024-09-19 19:21:00 +0300

    ruff

--- src/git_lines_graph/__init__.py
+++ src/git_lines_graph/__init__.py
@@ -1,10 +1,10 @@
 import argparse
-import git
 import os
-import sys
-import pandas as pd
+
+import git
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def main():
@@ -25,20 +25,17 @@ def main():
     data = []
     for i in reversed(list(repo.iter_commits(rev=branch))):
         diff = i.stats.total
-        data.append([i.committed_datetime.isoformat(),
-                     diff['insertions'],
-                     diff['deletions']])
+        data.append([i.committed_datetime.isoformat(), diff["insertions"], diff["deletions"]])
 
     data = pd.DataFrame(data, columns=["date", "add", "remove"])
-    data['delta'] = data['add'] - data['remove']
-    data['total'] = data.delta.cumsum()
+    data["delta"] = data["add"] - data["remove"]
+    data["total"] = data.delta.cumsum()
     data.date = pd.to_datetime(data.date)
-    data.set_index(['date'], inplace=True)
+    data.set_index(["date"], inplace=True)
 
     with plt.xkcd():
-        plt.figure("Code Lines Progress in project {}".
-                   format(os.path.basename(git_dir)))
+        plt.figure("Code Lines Progress in project {}".format(os.path.basename(git_dir)))
         plt.ylabel("# of lines")
-        ax = data['total'].plot()
-        ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+        ax = data["total"].plot()
+        ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter("{x:,.0f}"))
         plt.show()
